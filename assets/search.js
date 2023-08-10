1e71c4db11a8274b146d940045730ac1a58ff226
Masonb18 <bauer.mason01@gmail.com>
2023-08-09 20:28:58 -0500

    changed the search file a lot

--- assets/search.js
+++ assets/search.js
@@ -1,118 +1,35 @@
+var searchFormEl = document.querySelector('#form-submit');
 var weatherKey = "e90364e4c331c56af9a56244cf50c6e8";
-var city = "{city name}";
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" 
-var resultTextEl = document.querySelector("document")
-
+var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=';
+var resultTextEl = document.querySelector("document");
+var citySearch = document.querySelector('#city');
 var cardDisplay = document.getElementById('central-div');
 var subBtn = document.getElementById('submit-btn');
 
-
-
-function callAPI(queryURL){
-    var requestURL = queryURL  + city + "&appid=" + weatherKey;
-
+function callAPI(city) {
+    var requestURL = queryURL + city + "&appid=" + weatherKey;
     fetch(requestURL)
-    .then(function (response){
-        return response.json();
-    })
-    .then(function (data) {
+    .then(response => response.json())
+    .then(data => {
         console.log(data);
+        // Handle the data here
     })
-
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
 }
-
-function getWeather(){
-    var weatherParams =document.location.input.split('&')
-
-    var query = weatherParams[0].split('=').pop();
-    var format = weatherParams[0].split('=').pop();
-
-    searchApi(query, format);
-
-}
-
-function showResults(resultWeath) {
-    console.log(resultWeath)
-
-    var todayCard = document.createElement('div');
-    todayCard.classList.add('card', 'bg-light', 'text-dark', 'm-3', 'p-3');
-
-}
-
-
-
-
-
-
-
-
-
-var searchFormEl = document.querySelector('#form-submit')
-var weatherKey = "e90364e4c331c56af9a56244cf50c6e8";
-var city 
-var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q='
-var forcastURL = ''
-var resultTextEl = document.querySelector("document")
-var citySearch = document.querySelector('#city')
-var cardDisplay = document.getElementById('central-div');
-var subBtn = document.getElementById('submit-btn');
-
-
-
-
-
-function callAPI(){
-    var requestURL = queryURL  + city + "&appid=" + weatherKey;
-    fetch(requestURL)
-    .then(data => data.json(data))
-        console.log(jsonData);
-    }
-
-  
-    // fetch(requestURL)
-    // .then(function (response){
-    //     setData(response.data)
-    // })
-    // .then(function (data) {
-    //     console.log(data);
-    // })
-
-
 
 function handleFormSubmit(event) {
     event.preventDefault();
 
-    var searchInput = document.querySelector("#city").value;
-    var searchSubmit = document.querySelector("#submit-btn").value;
+    var searchInput = citySearch.value;
 
     if (!searchInput) {
-        console.error("Please enter valid city")
- 
+        console.error("Please enter a valid city");
+        return;
     }
 
-    var queryString = queryURL + searchSubmit + '&format=' + searchInput;
-    location.assign(queryString);
-    console.log(data);
-
+    callAPI(searchInput);
 }
-
-
-// function getWeather(){
-//     var weatherParams =document.location.input.split('&')
-
-//     var query = weatherParams[0].split('=').pop();
-//     var format = weatherParams[0].split('=').pop();
-
-//     searchApi(query, format);
-
-// }
-
-// function showResults(resultWeath) {
-//     console.log(resultWeath)
-
-//     var todayCard = document.createElement('div');
-//     todayCard.classList.add('card', 'bg-light', 'text-dark', 'm-3', 'p-3');
-
-// }
 
 searchFormEl.addEventListener("submit", handleFormSubmit);
